cdd3c84ecc84d06dcd5e2187183efcfddb1d1d1d
samfrack <89489598+samfrack@users.noreply.github.com>
2022-05-05 17:43:04 -0500

    Lesson 4.2.2 Complete

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerrb;
+    private GameObject focalPoint;
+    public float speed = 5.0f;
+    
+    // Instantiates Rigidbody 
+    void Start()
+    {
+        playerrb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Moves player forward based off vertical input buttons
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        playerrb.AddForce(focalPoint.transform.forward * speed * forwardInput);
+    }
+}
